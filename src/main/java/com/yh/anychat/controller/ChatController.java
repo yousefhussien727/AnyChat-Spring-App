a84cc46bd9d27556df2d6f1e32e096c0b976715a
yousefhussien727 <yousef.hu@outlook.com>
2025-02-23 21:19:52 +0200

    Update controller & FE to fetch chat from DB

--- src/main/java/com/yh/anychat/controller/ChatController.java
+++ src/main/java/com/yh/anychat/controller/ChatController.java
@@ -1,26 +1,40 @@
 package com.yh.anychat.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.yh.anychat.model.ChatMessage;
-
+import com.yh.anychat.service.ChatService;
 
 @Controller
 public class ChatController {
-    
+    private final ChatService chatService;
+
+    public ChatController(ChatService chatService) {
+        this.chatService = chatService;
+    }
 
     @MessageMapping("/sendMessage")
     @SendTo("/topic/messages")
     public ChatMessage sendMessage(ChatMessage chatMessage) {
-        return chatMessage;
+        return chatService.saveChatMessage(chatMessage); // will return the saved chat message
+        // return chatMessage;
     }
 
     @GetMapping("chat")
     public String chat() {
         return "chat";
     }
-    
+
+    @GetMapping("/chat/history")
+    public ResponseEntity<List<ChatMessage>> viewAllChatMessages() {
+        List<ChatMessage> chatMessages = chatService.getAllChatMessages();
+        return ResponseEntity.ok(chatMessages);
+    }
+
 }
