ac25d618b7d7aa81eceabe31a4eb87d40005b19c
yousefhussien727 <yousef.hu@outlook.com>
2025-02-23 21:04:43 +0200

    Update Model for DB

--- src/main/java/com/yh/anychat/model/ChatMessage.java
+++ src/main/java/com/yh/anychat/model/ChatMessage.java
@@ -1,19 +1,30 @@
 package com.yh.anychat.model;
 
+import java.time.LocalDateTime;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Entity
 @Data
-@NoArgsConstructor
 public class ChatMessage {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String sender;
     private String content;
+    private LocalDateTime timestamp;
+
+    public ChatMessage() {
+        this.timestamp = LocalDateTime.now();
+    }
+
+    public ChatMessage(String sender, String content) {
+        this.sender = sender;
+        this.content = content;
+        this.timestamp = LocalDateTime.now();
+    }
 }
