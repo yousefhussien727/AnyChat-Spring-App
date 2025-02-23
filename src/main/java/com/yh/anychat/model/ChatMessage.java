05cd851889430d3ffbd14c488365f53667c03ab5
yousefhussien727 <yousef.hu@outlook.com>
2025-02-23 21:34:19 +0200

    Apply JPA Entity Best Practices

--- src/main/java/com/yh/anychat/model/ChatMessage.java
+++ src/main/java/com/yh/anychat/model/ChatMessage.java
@@ -2,21 +2,30 @@ package com.yh.anychat.model;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
+@Table(name = "CHAT_MESSAGE") // Define table name explicitly
 @Data
 public class ChatMessage {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String sender;
+
+    @Column(nullable = false)
     private String content;
+
     private LocalDateTime timestamp;
+
 
     public ChatMessage() {
         this.timestamp = LocalDateTime.now();
