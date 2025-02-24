8729a23083a904a8b8836985e74f05f3dcf225de
yousefhussien727 <yousef.hu@outlook.com>
2025-02-24 15:16:18 +0200

    Apply Best Practices for Websocket

--- src/main/java/com/yh/anychat/controller/ChatController.java
+++ src/main/java/com/yh/anychat/controller/ChatController.java
@@ -6,31 +6,32 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.yh.anychat.model.ChatMessage;
 import com.yh.anychat.service.ChatService;
 
+import lombok.AllArgsConstructor;
+
 @Controller
+@CrossOrigin("*") // Allows frontend access
+@AllArgsConstructor // Automatically generates the constructor
 public class ChatController {
     private final ChatService chatService;
-
-    public ChatController(ChatService chatService) {
-        this.chatService = chatService;
-    }
 
     @MessageMapping("/sendMessage")
     @SendTo("/topic/messages")
     public ChatMessage sendMessage(ChatMessage chatMessage) {
         return chatService.saveChatMessage(chatMessage); // will return the saved chat message
-        // return chatMessage;
     }
 
     @GetMapping("chat")
     public String chat() {
         return "chat";
-    }
+    }   
 
+    // Fetch chat history
     @GetMapping("/chat/history")
     public ResponseEntity<List<ChatMessage>> viewAllChatMessages() {
         List<ChatMessage> chatMessages = chatService.getAllChatMessages();
