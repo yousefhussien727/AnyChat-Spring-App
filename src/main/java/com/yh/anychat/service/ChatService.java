0d1a59d74f601f1e18567acdabe7ceda4e50ef4c
yousefhussien727 <yousef.hu@outlook.com>
2025-02-24 15:11:11 +0200

    Apply Class Best Practices

--- src/main/java/com/yh/anychat/service/ChatService.java
+++ src/main/java/com/yh/anychat/service/ChatService.java
@@ -3,22 +3,25 @@ package com.yh.anychat.service;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.yh.anychat.model.ChatMessage;
 import com.yh.anychat.repository.ChatRepository;
 
+import lombok.AllArgsConstructor;
+
 @Service
+@AllArgsConstructor // Automatically generates the constructor
 public class ChatService {
     private final ChatRepository chatRepository;
 
-    public ChatService(ChatRepository chatRepository) {
-        this.chatRepository = chatRepository;
-    }
-
+    // Fetching all chat messages
     public List<ChatMessage> getAllChatMessages() {
         return chatRepository.findAll();
     }
 
+    // Saving chat message from Sender
+    @Transactional // for Data Integrity
     public ChatMessage saveChatMessage(ChatMessage chatMessage) {
         return chatRepository.save(chatMessage);
     }
